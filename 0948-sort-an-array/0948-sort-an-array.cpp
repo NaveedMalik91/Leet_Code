4ce294050865aef69c8b359b580dfbbc10c0d70f
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-15 22:43:37 +0530

    Time: 186 ms (48.58%), Space: 69.3 MB (96.32%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,61 +1,34 @@
 class Solution {
 public:
-void merge(vector<int> &v, vector<int> &temp, int start, int mid, int end)
-{
-    //first array = start -> mid
-    //second array = mid+1 -> end
-    //merged array = start -> end
-    int i = start,j = mid+1;
-    int  k = start;
-    while(i<= mid && j<= end)
-    {
-        if(v[i]<v[j])
-        {
-            temp[k++] = v[i++];
-            
-        }
-        else
-        {
-            temp[k++] = v[j++];
+    void mergeInplace(vector<int>& v, int start, int mid, int end) {
+        int total_len = end - start + 1;
+        int gap =(total_len/2)+(total_len%2);
+        while (gap > 0) {
+            int i = start;
+            int j = gap + start;
+            while (j <= end) {
+                if (v[i] > v[j]) {
+                    swap(v[i], v[j]);
+                }
+                ++i;
+                ++j;
+            }
+            gap = gap<=1?0:(gap/2)+(gap%2);
         }
     }
-
-    //Agr arays ka size differ ho
-    //toh waaki ke elements ko handle kro
-    while(i <=mid)
-    {
-        temp[k++] = v[i++];
+    void mergeSort(vector<int>& v, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        // Divide
+        mergeSort(v, start, mid);
+        mergeSort(v, mid + 1, end);
+        // Conquer or merge sorted arrays
+        mergeInplace(v, start, mid, end);
     }
-    while(j <= end)
-    {
-        temp[k++] = v[j++];
-    }
-
-    // merged data ko copy karo temp vector se original vector k amdhar
-    while(start <= end)
-    {
-        v[start] = temp[start];
-        start++; 
-    }
-}
-void mergeSort(vector<int> &v, int start,vector<int> &temp,int end)
-{
-    if(start >= end)
-    {
-        return;
-    }
-    int mid = (start+end)/2;
-    //Divide
-    mergeSort(v,start,temp,mid);
-    mergeSort(v,mid+1,temp,end);
-    //Conquer or merge arrays
-    merge(v,temp,start,mid,end);
-}
     vector<int> sortArray(vector<int>& nums) {
-        //temp storage to store sorted array
-        vector<int> temp(nums.size(),0);
-        mergeSort(nums,0,temp,nums.size()-1);
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
-        
     }
 };
