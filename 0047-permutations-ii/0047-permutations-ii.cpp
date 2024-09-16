65bbaacb79dcb473bfbb161f9542b237976dae51
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-16 13:45:14 +0530

    Time: 7 ms (51.39%), Space: 14.6 MB (22.31%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void Helper(vector<int>& nums, vector<vector<int>>& ans, int index) {
+        if (index >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        unordered_map<int,bool> visited;
+        for (int i = index; i < nums.size(); i++) {
+            if(visited.find(nums[i]) != visited.end())
+            {
+                continue;
+            }
+            visited[nums[i]] = true;
+            swap(nums[index], nums[i]);
+            Helper(nums, ans, index + 1);
+            swap(nums[index], nums[i]); // Backtrack to restore original state
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        Helper(nums, ans, 0);
+        return ans;
+    }
+};
