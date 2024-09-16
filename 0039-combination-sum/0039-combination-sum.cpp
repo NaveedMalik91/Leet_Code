6b3288aa94fded64a1c86e56add157a9a56aa0be
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-16 10:12:25 +0530

    Time: 4 ms (71.11%), Space: 12.5 MB (92.81%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void combination_sum_helper(vector<int> &candidates,int target,vector<int> &vec,vector<vector<int>> &ans,int index)
+    {
+        //base cas:- pehle element ko consider karte karte target
+        // zero yaa negative ho gya
+        if(target == 0)
+        {
+            ans.push_back(vec);
+            return;
+            
+        }
+        if(target < 0)
+        {
+            return;
+        }
+        for(int i = index; i<candidates.size();i++)
+        {
+            vec.push_back(candidates[i]);
+            combination_sum_helper(candidates,target-candidates[i],vec,ans,i);
+            //negative hone pe backtrack karenge aur jo b current element hai use consider nahi karenge
+            vec.pop_back();
+
+        }
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans; //fro storing the actual vector of values that make up target sum
+        vector<int> curr; //For including all values of candidates each time or exclude them
+        //Ek index se multiple index ki possibilities check karne k liye
+        combination_sum_helper(candidates,target,curr,ans,0); 
+        return ans;
+    }
+};
