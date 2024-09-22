aa24b847e661fb5d3f5ce4018b16796820a6a9d9
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-22 17:26:09 +0530

    Time: 8 ms (28.99%), Space: 12.9 MB (79.33%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,17 +13,36 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = NULL;
         ListNode* curr = head;
-        while(curr!=NULL)
-        {
-            ListNode* next_node = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next_node;
+        // while(curr!=NULL)
+        // {
+        //     ListNode* next_node = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = next_node;
             
 
-        }
-        head = prev;
-        return head;
+        // }
+        // head = prev;
+        return reverseusingRec(curr,prev);
+        
         
     }
+
+    /* Recursive Solution*/
+    ListNode* reverseusingRec(ListNode* curr, ListNode* prev) {
+        if(curr == NULL)
+        {
+            return prev;
+        }
+        ListNode* curr_next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = curr_next;
+        ListNode* reverse_ans = reverseusingRec(curr,prev);
+        return reverse_ans;
+
+    }
+
+    
+
 };
