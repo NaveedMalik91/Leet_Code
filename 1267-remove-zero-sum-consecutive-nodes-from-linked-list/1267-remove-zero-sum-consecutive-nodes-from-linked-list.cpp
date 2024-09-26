7d72b86cd63ad4b001a85427d5385f6b4a03ee18
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-26 20:03:46 +0530

    Time: 7 ms (72.42%), Space: 14.6 MB (89.93%) - LeetHub

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/1267-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* helper(ListNode* head) {
+        if (!head)
+        {
+            return NULL;
+        }
+        if (!head->next) return (head->val == 0) ? nullptr : head; // Handle single node case
+       
+        ListNode* temp = head;
+        ListNode* headNode = head;  // Initially, keep the head as headNode
+        int sum = 0;
+        while (temp != NULL) {
+            sum += temp->val;
+            if (sum == 0) {
+                headNode = temp->next;  // Update headNode when sum becomes zero
+            }
+            temp = temp->next;
+        }
+        // agr kuch nodes milke zero bna diye hai aur head ab change 
+
+        if (headNode != head) {  
+            return helper(headNode);  // Recur for the new headNode
+        } else {
+            head->next = helper(head->next);  // Recur for the next nodes
+            return head;
+        }
+    }
+
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        return helper(head);
+    }
+};
