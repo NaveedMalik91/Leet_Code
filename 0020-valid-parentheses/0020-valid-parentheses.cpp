b346956ce97a968abe543c13be3a4a85cac2cce7
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-28 12:37:39 +0530

    Time: 3 ms (40.77%), Space: 7.8 MB (57.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0;i<s.length();i++)
+        {
+            char ch  = s[i];
+            if(ch == '['||ch == '{'||ch == '(')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                    //agr closing bracket hai toh match dekho
+                    if(ch == ')' && st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else if(ch == '}' && st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else if(ch == ']' && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    //no match
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
