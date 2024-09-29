4f7035547dd24c5c3e2045e12f06029b92671404
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-09-29 11:23:23 +0530

    Time: 131 ms (23.4%), Space: 90.5 MB (16.22%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
+    //Yeh hume previous smaller element ke indices dega kisi particular ele ke liye
     vector<int> PSE(vector<int> arr) {
         vector<int> ans(arr.size());
         stack<int> st;
         st.push(-1);
-        // left to right jaa rahe hai hai
-        // aur curr se piche wale ele compare kr rahe hai
+        
         for (int i = 0; i < arr.size(); i++) {
             int curr = arr[i];
             while (st.top() != -1 && arr[st.top()] >= curr) {
                 st.pop();
             }
-            // smaller element mil gaya
+           
             ans[i] = st.top();
 
             st.push(i);
@@ -19,6 +19,7 @@ public:
         return ans;
     }
 
+    //Yeh hume next smaller element ke indices dega
     vector<int> NSE(vector<int> arr) {
         vector<int> ans(arr.size());
         stack<int> st;
@@ -28,16 +29,17 @@ public:
             while (st.top() != -1 &&arr[st.top()] >= curr) {
                 st.pop();
             }
-            // smaller element mil gaya
             ans[i] = st.top();
-            // curr ele ko stack me daale
-            // woh b kisi ele ka smaller ho sakta hai
             st.push(i);
         }
         return ans;
     }
+
+    
     int largestRectangleArea(vector<int>& heights) {
+        //store next smaller ele indices
         vector<int> next = NSE(heights);
+        //agr -1 hai toh sizek baraabar kardo
         for(int i = 0;i<next.size();i++)
         {
             if(next[i] == -1)
@@ -45,13 +47,16 @@ public:
                 next[i] = next.size();
             }
         }
+        //Store prev smaller ele indices
         vector<int> prev = PSE(heights);
         int max_area = INT_MIN;
         for(int i = 0;i<heights.size();i++)
         {
             
             int len = heights[i];
-            int width = next[i]-prev[i]-1; //formula
+            //prev aur next smaller ele indices ko use krke 
+            //formuka tayaar hua
+            int width = next[i]-prev[i]-1; 
             int area = len*width;
             max_area = max(max_area,area);
         }
