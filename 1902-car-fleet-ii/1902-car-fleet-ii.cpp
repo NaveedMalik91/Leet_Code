867c9b13b459685ed51ee709d5e1eeee099a7dc5
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-10-02 16:06:58 +0530

    Time: 393 ms (47.61%), Space: 134.9 MB (36.02%) - LeetHub

---
+++ 1902-car-fleet-ii/1902-car-fleet-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<double> getCollisionTimes(vector<vector<int>>& cars) {
+        //store collision time
+        vector<double> ans(cars.size(),-1);
+        stack<int> st;
+        for(int i = cars.size()-1;i>=0;--i)
+        {
+            while(!st.empty() && cars[st.top()][1] >= cars[i][1])
+            {
+                st.pop();
+            } 
+            while(!st.empty())
+            {
+                double coltime = (double)(cars[st.top()][0] - cars[i][0]) / (cars[i][1]-cars[st.top()][1]);
+                
+                if(ans[st.top()] == -1 || coltime <= ans[st.top()])
+                {
+                    ans[i] = coltime;
+                    break;
+                }
+                st.pop();
+                
+            }
+            st.push(i);
+
+        }
+
+        return ans;
+
+
+        
+    }
+};
