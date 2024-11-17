859c391e11068a09aebcf0d52ccabb49caf05376
Naveed malik <134996671+NaveedMalik91@users.noreply.github.com>
2024-11-17 11:42:51 +0530

    Time: 0 ms (100%), Space: 21.7 MB (89.48%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,49 +11,23 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root)
-    {
-        //base case
-        if(root == NULL)
-        {
+    bool balanced = true;
+    int height(TreeNode* root){
+        
+        if(root == NULL){
             return 0;
         }
-        int left_h = height(root->left);
-        int right_h = height(root->right);
-        int ans = max(left_h,right_h)+1;
-        return ans;
+        int left = height(root->left);
+        int right = height(root->right);
+        int height = max(left,right);
+        if(balanced && abs(left-right) > 1){
+            balanced = false;
+        }
+        return max(left,right)+1;
+
     }
     bool isBalanced(TreeNode* root) {
-        //base case
-        if(root == NULL)
-        {
-            return true;
-        }
-        //ek case
-        //curr node ki left height nikaalo and right height nikaalo
-        //then minus krke dekho  <1 hai agr hai toh true
-        int left_h = height(root->left);
-        int right_h = height(root->right);
-        int diff = abs(left_h - right_h);
-        //kya left aur right height ka diff 1 hai
-        bool currnode_ans = (diff <= 1);
-
-        //kya left subtree balance hai
-        bool left_ans = isBalanced(root->left);
-        //kya right subtree balance hai
-        bool right_ans = isBalanced(root->right);
-
-        //agr 3 cases hau toh true
-        if(currnode_ans && left_ans && right_ans)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        
-
-        
+        height(root);
+        return balanced;
     }
 };
